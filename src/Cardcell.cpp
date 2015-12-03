8c795f1461ea3ef783606e867595662467bbbb49
Anthony Varghese <anthonyvar@gmail.com>
2015-12-03 15:29:40 -0600

    Set up Cardcell main program .gitignore was automatically modified

---
+++ src/Cardcell.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Cardcell.cpp
+// Author      : Anthony Varghese
+// Version     :
+// Copyright   : All rights reserved by author(s)
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Simple cardiac cell!" << endl; // prints Simple cardiac cell!
+	return 0;
+}
